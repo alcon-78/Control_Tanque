65df5979f4d5ea175001d21924ace30164d37407
Cristian O. Viola <cristian_viola@hotmail.com.ar>
2015-01-30 00:39:59 -0300

    Día 2

--- tanque.pde
+++ tanque.pde
@@ -8,21 +8,62 @@ Programa Host [Python]: https://github.com/fandres/pyqwt-Temperatura-pinguino/tr
 
 #include <stdlib.h>   // la necesitaremos para la función "itoa()"
 
-char enviado[64];   // Cadena a ser enviada.
+char enviado[64];   // Cadena enviada a pyQt.
 int entero = 0;     // Donde almacenamos temporalmente el valor leido.
 
+char recibido[64];  // Cadena recibida desde pyQt 
+u8 recibirbyte;
+
+ 
 //-------------------------setup()---------------------//
 void setup()
 {
-    pinMode(14, INPUT);  // pin 13 de la board pinguino como salida.
+    pinMode(10, OUTPUT); // pin 10 led amarillo (nivel minimo)
+    pinMode(11, OUTPUT); // pin 11 led verde (encendido OK / titilante falla)
+    pinMode(12, OUTPUT); // pin 12 led rojo (rele termico del motor bomba de llenado activado)
+    pinMode(13, INPUT);  // pin 13 boton ON/OFF bomba de llenado
+    pinMode(14, INPUT);  // pin 14 potenciometro simulación de nivel.
+    pinMode(17, INPUT);  // pin 17 contacto auxiliar rele termico (97-98) motor de llenado
+    digitalWrite(11, LOW); // apagar led verde
 }
 
 //-------------------------loop()---------------------//
 void loop()
 {
+    //
+    //
+    //
     
     entero = analogRead(14);    // Leyendo del Pin13
     itoa(entero,enviado,10);    // Conversión de int a string
     BULK.write(enviado, 5);    // Enviado los datos al host
     delay(100);                // Esperamos 100 milisegundos. 
+    
+    //
+    //
+    //
+    
+    recibirbyte=0;
+    if(BULK.available())
+        do {
+                recibirbyte = BULK.read(recibido);
+                
+            } while(recibirbyte == 0);
+            recibido[recibirbyte] = 0;
+       
+            if(recibirbyte > 0){
+                 // Establecer estados (ON/OFF) led de sealizacion del sistema             
+                 if (recibirbyte == '1') digitalWrite(10,HIGH);
+                 else if (recibirbyte == '2') digitalWrite(10,LOW);
+                 else if (recibirbyte == '3') digitalWrite(11,HIGH);
+                 else if (recibirbyte == '4') digitalWrite(11,LOW);
+                 else if (recibirbyte == '5') digitalWrite(12,HIGH);
+                 else if (recibirbyte == '6') digitalWrite(12,LOW);
+                 else{
+                 // Cualquier otro valor
+                    digitalWrite(10,LOW);
+                    digitalWrite(11,LOW);
+                    digitalWrite(12,LOW);             
+                  }
+             }
 }
--- tanque.py
+++ tanque.py
@@ -3,6 +3,7 @@
 Created on Thu Mar 27 18:28:11 2014
 << Pinguino by Marin Purgar (marin.purgar@gmail.com)                         >>
 @author: fAnDrEs (fabian.salamanca@openmailbox.org)
+Modificada por Cristian O. Viola 2014/2015
 """
 
 import sys
@@ -29,104 +30,140 @@ class Principal(QtGui.QFrame):
         #Centramos el frame
         self.centrar()
 
-        # Conectamos la senal de click del boton (boton_conectar) con el metodo inicio
-        self.connect(self.ui.boton_conectar, QtCore.SIGNAL("clicked()"), self.inicio)
+        # Conectamos la senal de click de (boton_conectar) con el metodo inicio
+        self.connect(self.ui.boton_conectar, QtCore.SIGNAL("clicked()"),
+             self.inicio)
+        # Conectamos la senal de click del boton (bt_llenado) con el metodo
+        self.connect(self.ui.bt_llenado, QtCore.SIGNAL("toggled()"),
+            self.on_bt_llenado_toggled)
+        # Conectamos la senal de click del boton (bt_vaciado) con el metodo
+        self.connect(self.ui.bt_vaciado, QtCore.SIGNAL("toggled()"),
+            self.on_bt_vaciado_toggled)
+
+        # Configurar nivel maximo y minimo del tanque
+        self.connect(self.ui.bt_confirmar, QtCore.SIGNAL("toggled()"),
+            self.on_bt_confirmar_toggled)
 
     def inicio(self):
         # Conexcion pinguino
         self.pinguino = Pinguino()
-        if self.pinguino.pinguinoOpen() == None:
-            print >> sys.stderr, "Unable to open Pinguino device!"
-            self.pinguino.pinguinoClose() # Cerrar aplicacion de manera correcta.
+        if (self.pinguino.pinguinoOpen() == None):
+            self.ui.textObs.append('El dispositivo no esta disponible. Revise la conexion Pinguino <--> PC')
+            # Cerrar aplicacion de manera correcta.
+            self.pinguino.pinguinoClose()
         else:
-            # Activamos en la GUI el elemento QWT [qwtThermo] y desactivamos el boton conectar.
-            #self.ui.qwt_Thermo.setEnabled(True)
+            # Activamos en la GUI el elemento y desactivamos boton conectar.
+            self.ui.textObs.clear()
+            self.ui.textObs.append('Conexion exitosa!')
             self.ui.boton_conectar.setEnabled(False)
+            self.ui.bt_llenado.setEnabled(True)
+            self.ui.bt_llenado.setCheckable(True)
+            self.ui.bt_vaciado.setEnabled(True)
+            self.ui.bt_vaciado.setCheckable(True)
 
             # Timer
             self.timer = QtCore.QTimer()
-            self.timer.timeout.connect(self.termperatura)
-            self.timer.start(50) #se ejecutará la self.temperatura() cada 500 mili segundos
-
-
-
-        """ Metodos para qwtTermo """
-        #self.ui.qwt_Thermo.setValue(40) # Asigna valor al elemento qwtThermo
-        #self.ui.qwt_Thermo.setPipeWidth(20) # Cambia anchura del pipe(barra termomentro)
-        #self.ui.qwt_Thermo.setAlarmLevel(50) Cambia el nivel de alarma [entero]
-        #self.ui.qwt_Thermo.setAlarmColor(QtGui.QColor(0, 177, 0)) # cambia el Color de la ararma
-        #print self.ui.qwt_Thermo.alarmEnabled() # retorna True si existe alarma
-        #print self.ui.qwt_Thermo.alarmLevel() # Retorna el valor en el cual se activa la alarma
-
-        # Cambio alarm Brush de alarm
-        #brush = QtGui.QBrush(QtGui.QColor(0 , 0, 170))
-        #brush.setStyle(QtCore.Qt.SolidPattern)
-        #self.ui.qwt_Thermo.setAlarmBrush(brush)
-
-        """Metodos QLCDNumber """
-        #self.ui.lcdNumber_1.display(60) # Muestra un valor en el LCD
-        # Sinal = overFlow
-        #print self.ui.lcdNumber_1.value() # Retorta valor de QLCDNumber
-        #print self.ui.lcdNumber_1.intValue() # Recorta valor de QLCDNumber en entero
-        #self.ui.lcdNumber_1.setDisabled(True)# Desactiva el LCD
-        #self.ui.lcdNumber_1.setNumDigits(1) # Numero de digitos que muestra el LCD
-        #print self.ui.lcdNumber_1.numDigits()  # Numero de digitos que esta mostrando el LCD
-
-
+            self.timer.timeout.connect(self.nivel)
+            self.timer.start(50)  # se ejecutará cada 500 mili segundos
 
 #-------------------------------------------------------------------------------
     def centrar(self):
-            screen = QtGui.QDesktopWidget().screenGeometry() # obtiene resolucion del usuario
-            size =  self.geometry()
-            self.move((screen.width()-size.width())/2, (screen.height()-size.height())/2)
+            # obtiene resolucion del usuario
+            screen = QtGui.QDesktopWidget().screenGeometry()
+            size = self.geometry()
+            self.move((screen.width() - size.width()) / 2, (screen.height() -
+            size.height()) / 2)
 
 #-------------------------------------------------------------------------------
-    def termperatura(self):
-        self.INTERVAL = 100 # intervalo (tiempo) de lectura
+    @QtCore.pyqtSlot()
+    def on_bt_llenado_pressed(self):  # si presiona el boton,
+        self.ui.id_llenado.setStyleSheet("background-color: white;")
+        self.ui.bt_llenado.setText("  ON")
+
+    def on_bt_llenado_toggled(self, checked):
+        if checked:
+            self.rowOverride = True
+            self.ui.bt_llenado.setText("  OFF")
+            self.ui.id_llenado.setStyleSheet("background-color: green;")
+        elif not checked:
+            self.rowOverride = False
+#-------------------------------------------------------------------------------
+    @QtCore.pyqtSlot()
+    def on_bt_vaciado_pressed(self):  # si presiona el boton,
+        self.ui.id_vaciado.setStyleSheet("background-color: white;")
+        self.ui.bt_vaciado.setText("  ON")
+
+    def on_bt_vaciado_toggled(self, checked):
+        if checked:
+            self.rowOverride = True
+            self.ui.bt_vaciado.setText("  OFF")
+            self.ui.id_vaciado.setStyleSheet("background-color: green;")
+        elif not checked:
+            self.rowOverride = False
+#-------------------------------------------------------------------------------
+    @QtCore.pyqtSlot()
+    def on_bt_confirmar_pressed(self):  # si presiona el boton,
+        self.ui.bt_confirmar.setText("Confirmar")
+        self.ui.lcdMaximo.setEnabled(True)
+        self.ui.lcdMinimo.setEnabled(True)
+        self.ui.sliderMax.setEnabled(True)
+        self.ui.sliderMin.setEnabled(True)
+
+    def on_bt_confirmar_toggled(self, checked):
+        if checked:
+            self.rowOverride = True
+            self.ui.bt_confirmar.setText("Configurar")
+            self.ui.lcdMaximo.setEnabled(False)
+            self.ui.lcdMinimo.setEnabled(False)
+            self.ui.sliderMax.setEnabled(False)
+            self.ui.sliderMin.setEnabled(False)
+        elif not checked:
+            self.rowOverride = False
+
+#-------------------------------------------------------------------------------
+    def nivel(self):
+        self.INTERVAL = 100  # intervalo (tiempo) de lectura
         deg = unichr(176).encode("utf-8")
         self.myString = ''
 
         # Obtiene los datos de la targeta Pinguino
-        try :
+        try:
             for i in self.pinguino.pinguinoRead(5, self.INTERVAL):
                 if i > 47 and i < 58:    # ANSI 0,1,2,...,9
                     #print "valor actual",i # Debug
                     self.myString += chr(i)
-                    #print "actual caracter",chr(i) # Debug
-                    #print "total ",self.myString # Debug
 
         except usb.USBError as err:
             pass
 
-        #print  "myString completo:",self.myString  #debug
-        #print float(self.myString)
-        #print type(self.myString) #debug
-        #print int(self.myString) #debug
         if len(self.myString) > 0:
-            #print "myString con longitud" #debug
-            self.termperatura = (int(self.myString)*100)/1023 # Conversión a Grados celcius.
-            #print "temperatura",self.termperatura #debug
-            self.ui.progressBar_Nivel.setValue(self.termperatura) # Asigna valor al elemento qwtThermo
-            #self.ui.lcdNumber_1.display(self.termperatura) # Muestra un valor en el LCD
-            # Calculamos la diferencia entre la alarma y el valor mostrado
-            #if (self.ui.lcdNumber_1.value() - self.ui.qwt_Thermo.alarmLevel())  > 0 :
-            #   self.ui.lcdNumber_2.display(self.ui.lcdNumber_1.value() - self.ui.qwt_Thermo.alarmLevel())
-
+            # Conversión a litros.
+            self.nivel = (int(self.myString) * 100) / 1023
+            # Asigna valor nivel tanque
+            self.ui.progressBar_Nivel.setValue(self.nivel)
+            if (self.nivel >= self.ui.sliderMax.value()):
+                self.ui.nivel_min.setStyleSheet("QFrame { background-color: #00FF00}")  # VERDE
+                self.ui.nivel_max.setStyleSheet("QFrame { background-color: #FF0000}")  # ROJO
+            elif (self.nivel <= self.ui.sliderMin.value()):
+                self.ui.nivel_min.setStyleSheet("QFrame { background-color: #FF0000}")  # ROJO
+                self.ui.nivel_max.setStyleSheet("QFrame { background-color: #00FF00}")  # VERDE
+            else:
+                self.ui.nivel_max.setStyleSheet("QFrame { background-color: #00FF00}")  # VERDE
+                self.ui.nivel_min.setStyleSheet("QFrame { background-color: #00FF00}")  # VERDE
 
 
 #-------------------------------------------------------------------------------
 # Pinguino Class by Marin Purgar (marin.purgar@gmail.com)
 #-------------------------------------------------------------------------------
-
 class Pinguino():
 
     VENDOR = 0x04D8
     PRODUCT = 0xFEAA
-    CONFIGURATION = 0x01 # if bootloader v4.x
+    CONFIGURATION = 0x01  # if bootloader v4.x
     #CONFIGURATION = 0x03 # if bootloader v2.x
     #print type(CONFIGURATION)
     INTERFACE = 0
-    ENDPOINT_IN = 0x81 # if bootloader v4.x
+    ENDPOINT_IN = 0x81  # if bootloader v4.x
     #ENDPOINT_IN = 0x82 # if bootloader v2.x
     ENDPOINT_OUT = 0x01
 
@@ -139,13 +176,13 @@ class Pinguino():
             for dev in bus.devices:
                 #print "vendor",dev.idVendor
                 #print "idproduct",dev.idProduct
-                if dev.idVendor == self.VENDOR and dev.idProduct == self.PRODUCT:
+                if dev.idVendor == self.VENDOR and dev. idProduct == self.PRODUCT:
                     self.device = dev
         return None
 
     def pinguinoOpen(self):
         if not self.device:
-            print >> sys.stderr, "Unable to find device!"
+            print >> sys.stderr, "El dispositivo no esta disponible!"
             return None
         try:
             self.handle = self.device.open()
@@ -163,15 +200,17 @@ class Pinguino():
             print >> sys.stderr, err
         self.handle, self.device = None, None
 
-    def pinguinoRead(self, length, timeout = 0):
+    def pinguinoRead(self, length, timeout=0):
         return self.handle.bulkRead(self.ENDPOINT_IN, length, timeout)
 
-    def pinguinoWrite(self, buffer, timeout = 0):
+    def pinguinoWrite(self, buffer, timeout=0):
         return self.handle.bulkWrite(self.ENDPOINT_OUT, buffer, timeout)
 
 #-------------------------------------------------------------------------------
 #
 #-------------------------------------------------------------------------------
+
+
 def main():
     # Nuevamente, esto es estándar, será igual en cada
     # aplicación que escribas
